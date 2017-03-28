da6695aaf1aed5c4984b8dc9c53f55a122166afb
Rafael Santos <rafael.dossantos@td.com>
2017-03-28 09:40:36 -0400

    More documentation and single instance for JsonUtil.objectMapper

--- src/main/java/com/github/rafasantos/matchandtrade/doc/util/JsonUtil.java
+++ src/main/java/com/github/rafasantos/matchandtrade/doc/util/JsonUtil.java
@@ -7,23 +7,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.rafasantos.matchandtrade.exception.DocMakerException;
 
+/**
+ * Utility class for common JSON manipulations
+ * 
+ * @author rafael.santos.bra@gmail.com
+ *
+ */
 public class JsonUtil {
 
+	private static ObjectMapper objectMapper;
 
-	public static String prettyJson(String responseBody) {
-		ObjectMapper objectMapper;
+	private static void init() {
 		objectMapper = new ObjectMapper();
 		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		objectMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);		
-		
+	}
+	
+	/**
+	 * Prettyfi a JSON string
+	 * 
+	 * @param json
+	 * @return pretty json
+	 */
+	public static String prettyJson(String json) {
+		if (objectMapper == null) {
+			init();
+		}
 		String result = null;
 		try {
-			Object json = objectMapper.readValue(responseBody, Object.class);
-			result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+			Object jsonObject = objectMapper.readValue(json, Object.class);
+			result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
 		} catch (IOException e) {
-			throw new DocMakerException("Not able to parse string to JSON: " + responseBody, e);
+			throw new DocMakerException("Not able to parse string to JSON: " + json, e);
 		}
 		return result;
 	}
+
 }
