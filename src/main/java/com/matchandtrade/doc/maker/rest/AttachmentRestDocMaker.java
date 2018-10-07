b8b1343a05c6642e992ac0d5e0e7ccfe70487c0c
Rafael Santos <rafael.santos.bra@gmail.com>
2018-10-07 19:13:39 -0400

    Refactored AttachmentRestDocMaker to use REST API Doc

--- src/main/java/com/matchandtrade/doc/maker/rest/AttachmentRestDocMaker.java
+++ src/main/java/com/matchandtrade/doc/maker/rest/AttachmentRestDocMaker.java
@@ -1,20 +1,17 @@
 
 package com.matchandtrade.doc.maker.rest;
 
-import java.io.File;
-
+import com.github.rafasantos.restapidoc.SpecificationFilter;
+import com.github.rafasantos.restapidoc.SpecificationParser;
 import com.github.rafasantos.restdocmaker.RestDocMaker;
-import com.github.rafasantos.restdocmaker.template.Snippet;
-import com.github.rafasantos.restdocmaker.template.SnippetFactory;
 import com.github.rafasantos.restdocmaker.template.TemplateUtil;
 import com.matchandtrade.doc.util.MatchAndTradeRestUtil;
 import com.matchandtrade.doc.util.PaginationTemplateUtil;
-
+import io.restassured.RestAssured;
 import io.restassured.builder.MultiPartSpecBuilder;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.Method;
 import io.restassured.specification.MultiPartSpecification;
-import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
 
 
 public class AttachmentRestDocMaker implements RestDocMaker {
@@ -28,17 +25,21 @@ public class AttachmentRestDocMaker implements RestDocMaker {
 	@Override
 	public String content() {
 		String template = TemplateUtil.buildTemplate(contentFilePath());
+
 		String filePath = AttachmentRestDocMaker.class.getClassLoader().getResource("image-landscape.png").getFile();
 		File file = new File(filePath);
 		MultiPartSpecification fileSpec = new MultiPartSpecBuilder(file).mimeType("image/png").fileName("my-image.png").build();
-		RequestSpecification attachmentRequest = new RequestSpecBuilder()
-				.addHeaders(MatchAndTradeRestUtil.getLastAuthorizationHeaderAsMap())
-				.addMultiPart(fileSpec)
-				.build();
-		Snippet attachmentSnippet = SnippetFactory.makeSnippet(Method.POST, attachmentRequest, MatchAndTradeRestUtil.attachmentsUrl());
-		attachmentSnippet.getResponse().then().statusCode(201);
-		template = TemplateUtil.replacePlaceholder(template, POST_PLACEHOLDER, attachmentSnippet.asHtml());
 
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.multiPart(fileSpec)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.post(MatchAndTradeRestUtil.attachmentsUrl());
+		parser.getResponse().then().statusCode(201);
+
+		template = TemplateUtil.replacePlaceholder(template, POST_PLACEHOLDER, parser.asHtmlSnippet());
 		template = PaginationTemplateUtil.replacePaginationTable(template);
 		return TemplateUtil.appendHeaderAndFooter(template);
 	}
