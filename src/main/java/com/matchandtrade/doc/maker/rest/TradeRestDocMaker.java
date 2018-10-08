27b9dcf6d0b80fce656c519c5a5174b4a5df1139
Rafael Santos <rafael.santos.bra@gmail.com>
2018-10-07 21:59:02 -0400

    Updated TradeRestDocMaker to user REST API Doc

--- src/main/java/com/matchandtrade/doc/maker/rest/TradeRestDocMaker.java
+++ src/main/java/com/matchandtrade/doc/maker/rest/TradeRestDocMaker.java
@@ -1,24 +1,18 @@
 package com.matchandtrade.doc.maker.rest;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.notNullValue;
-
-import java.util.Date;
-
+import com.github.rafasantos.restapidoc.SpecificationFilter;
+import com.github.rafasantos.restapidoc.SpecificationParser;
 import com.github.rafasantos.restdocmaker.RestDocMaker;
-import com.github.rafasantos.restdocmaker.template.Snippet;
-import com.github.rafasantos.restdocmaker.template.SnippetFactory;
 import com.github.rafasantos.restdocmaker.template.TemplateUtil;
+import com.matchandtrade.doc.maker.TemplateHelper;
 import com.matchandtrade.doc.util.MatchAndTradeRestUtil;
 import com.matchandtrade.doc.util.PaginationTemplateUtil;
 import com.matchandtrade.rest.v1.json.TradeJson;
 import com.matchandtrade.rest.v1.json.TradeJson.State;
-
-import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
-import io.restassured.specification.RequestSpecification;
+
+import static org.hamcrest.Matchers.*;
 
 
 public class TradeRestDocMaker implements RestDocMaker {
@@ -37,58 +31,118 @@ public class TradeRestDocMaker implements RestDocMaker {
 
 	@Override
 	public String content() {
-		String template = TemplateUtil.buildTemplate(contentFilePath());
-		SnippetFactory snippetFactory = new SnippetFactory(ContentType.JSON, MatchAndTradeRestUtil.getLastAuthorizationHeader());
-		
+		String template = TemplateHelper.buildTemplate(contentFilePath());
+
 		// TRADES_POST_PLACEHOLDER
-		TradeJson tradeJson = new TradeJson();
-		String tradeName = "Board games location TBD - " + new Date().getTime() + this.hashCode();
-		tradeJson.setName(tradeName);
-		tradeJson.setDescription("We will update the trade information once the description is defined");
-		Snippet postSnippet = snippetFactory.makeSnippet(Method.POST, tradeJson, MatchAndTradeRestUtil.tradesUrl() + "/");
-		postSnippet.getResponse().then().statusCode(201).and().body("", hasKey("tradeId"));
-		template = TemplateUtil.replacePlaceholder(template, TRADES_POST_PLACEHOLDER, postSnippet.asHtml());
-		tradeJson = postSnippet.getResponse().body().as(TradeJson.class);
-		
+		SpecificationParser postParser = parsePost();
+		template = TemplateHelper.replacePlaceholder(template, TRADES_POST_PLACEHOLDER, postParser.asHtmlSnippet());
+		TradeJson tradeJson = postParser.getResponse().body().as(TradeJson.class);
+
 		// TRADES_PUT_PLACEHOLDER
+		SpecificationParser putParser = parsePut(tradeJson);
+		template = TemplateUtil.replacePlaceholder(template, TRADES_PUT_PLACEHOLDER, putParser.asHtmlSnippet());
+		tradeJson = putParser.getResponse().body().as(TradeJson.class);
+
+		// TRADES_GET_PLACEHOLDER
+		SpecificationParser getByIdParser = parseGetById(tradeJson);
+		template = TemplateHelper.replacePlaceholder(template, TRADES_GET_PLACEHOLDER, getByIdParser.asHtmlSnippet());
+
+		// TRADES_SEARCH_PLACEHOLDER
+		SpecificationParser searchParser = parseSearch();
+		template = TemplateHelper.replacePlaceholder(template, TRADES_SEARCH_PLACEHOLDER, searchParser.asHtmlSnippet());
+
+		// TRADES_GET_ALL_PLACEHOLDER
+		SpecificationParser getAllParser = getAllParser();
+		template = TemplateHelper.replacePlaceholder(template, TRADES_GET_ALL_PLACEHOLDER, getAllParser.asHtmlSnippet());
+		
+		// TRADES_DELETE_PLACEHOLDER
+		SpecificationParser deleteParser = parseDelete(tradeJson);
+		template = TemplateHelper.replacePlaceholder(template, TRADES_DELETE_PLACEHOLDER, deleteParser.asHtmlSnippet());
+
+		template = PaginationTemplateUtil.replacePaginationTable(template);
+		return TemplateHelper.appendHeaderAndFooter(template);
+	}
+
+	private SpecificationParser parseDelete(TradeJson tradeJson) {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.delete(MatchAndTradeRestUtil.tradesUrl(tradeJson.getTradeId()));
+		parser.getResponse().then().statusCode(204);
+		return parser;
+	}
+
+	private SpecificationParser getAllParser() {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil
+			.getLastAuthorizationHeader())
+			.get(MatchAndTradeRestUtil.tradesUrl());
+		parser.getResponse().then().statusCode(200).and().headers("X-Pagination-Total-Count", notNullValue());
+		return parser;
+	}
+
+	private SpecificationParser parseSearch() {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given().filter(filter).header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+				.queryParam("_pageNumber", "1")
+				.queryParam("_pageSize", "3")
+				.get(MatchAndTradeRestUtil.tradesUrl());
+		parser.getResponse().then().statusCode(200);
+		return parser;
+	}
+
+	private SpecificationParser parseGetById(TradeJson tradeJson) {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given().filter(filter).header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+				.get(MatchAndTradeRestUtil.tradesUrl(tradeJson.getTradeId()));
+		parser.getResponse().then().statusCode(200).and().body("tradeId", equalTo(tradeJson.getTradeId()));
+		return parser;
+	}
+
+	private SpecificationParser parsePut(TradeJson tradeJson) {
 		Integer tradeId = tradeJson.getTradeId();
 		tradeJson.setTradeId(null); // Set as null because we do not want to display in the documentation
 		tradeJson.setLinks(null); // Set as null because we do not want to display in the documentation
-		tradeName = "Board games in Toronto - " + new Date().getTime() + this.hashCode(); 
+		String tradeName = "Board games in Toronto - " + System.currentTimeMillis() + "Updated";
 		tradeJson.setName(tradeName);
 		tradeJson.setDescription("The event will take place at Toronto Convention Center");
 		tradeJson.setState(State.MATCHING_ARTICLES);
-		Snippet putSnippet = snippetFactory.makeSnippet(Method.PUT, tradeJson, MatchAndTradeRestUtil.tradesUrl(tradeId));
-		putSnippet.getResponse().then().statusCode(200).and().body("name", equalTo(tradeJson.getName()));
-		template = TemplateUtil.replacePlaceholder(template, TRADES_PUT_PLACEHOLDER, putSnippet.asHtml());
-		
-		// TRADES_GET_PLACEHOLDER
-		Snippet getSnippet = snippetFactory.makeSnippet(MatchAndTradeRestUtil.tradesUrl(tradeId));
-		getSnippet.getRequest().then().statusCode(200).and().body("tradeId", equalTo(tradeId));
-		template = TemplateUtil.replacePlaceholder(template, TRADES_GET_PLACEHOLDER, getSnippet.asHtml());
-		
-		// TRADES_SEARCH_PLACEHOLDER
-		RequestSpecification searchRequest = new RequestSpecBuilder()
-				.addHeaders(MatchAndTradeRestUtil.getLastAuthorizationHeaderAsMap())
-				.addQueryParam("_pageNumber", "1")
-				.addQueryParam("_pageSize", "3")
-				.build();
-		Snippet searchSnippet = SnippetFactory.makeSnippet(Method.GET, searchRequest, MatchAndTradeRestUtil.tradesUrl()); 
-		searchSnippet.getResponse().then().statusCode(200);
-		template = TemplateUtil.replacePlaceholder(template, TRADES_SEARCH_PLACEHOLDER, searchSnippet.asHtml());
 
-		// TRADES_GET_ALL_PLACEHOLDER
-		Snippet getAllSnippet = snippetFactory.makeSnippet(MatchAndTradeRestUtil.tradesUrl());
-		getAllSnippet.getResponse().then().statusCode(200).and().headers("X-Pagination-Total-Count", notNullValue());
-		template = TemplateUtil.replacePlaceholder(template, TRADES_GET_ALL_PLACEHOLDER, getSnippet.asHtml());
-		
-		// TRADES_DELETE_PLACEHOLDER
-		Snippet deleteSnippet = snippetFactory.makeSnippet(Method.DELETE, MatchAndTradeRestUtil.tradesUrl(tradeId));
-		deleteSnippet.getResponse().then().statusCode(204);
-		template = TemplateUtil.replacePlaceholder(template, TRADES_DELETE_PLACEHOLDER, deleteSnippet.asHtml());
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+				.filter(filter)
+				.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+				.contentType(ContentType.JSON)
+				.body(tradeJson)
+				.put(MatchAndTradeRestUtil.tradesUrl(tradeId));
+		parser.getResponse().then().statusCode(200).and().body("name", equalTo(tradeName));
+		return parser;
+	}
 
-		template = PaginationTemplateUtil.replacePaginationTable(template);
-		return TemplateUtil.appendHeaderAndFooter(template);
+	private SpecificationParser parsePost() {
+		TradeJson tradeJson = new TradeJson();
+		String tradeName = "Board games location TBD - " + System.currentTimeMillis();
+		tradeJson.setName(tradeName);
+		tradeJson.setDescription("We will update the trade information once the description is defined");
+
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.contentType(ContentType.JSON)
+			.body(tradeJson)
+			.post(MatchAndTradeRestUtil.tradesUrl() + "/");
+		parser.getResponse().then().statusCode(201).and().body("", hasKey("tradeId"));
+		return parser;
 	}
 
 }
