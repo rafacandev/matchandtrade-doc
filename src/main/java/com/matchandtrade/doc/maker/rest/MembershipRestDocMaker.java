2b94db97c19e0ea5ec51072042de75ea23515a2d
Rafael Santos <rafael.santos.bra@gmail.com>
2018-10-09 00:27:29 -0400

    Refactored MembershipRestDocMaker to use REST API Doc

--- src/main/java/com/matchandtrade/doc/maker/rest/MembershipRestDocMaker.java
+++ src/main/java/com/matchandtrade/doc/maker/rest/MembershipRestDocMaker.java
@@ -1,25 +1,24 @@
 package com.matchandtrade.doc.maker.rest;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-
-import java.util.Date;
-
+import com.github.rafasantos.restapidoc.SpecificationFilter;
+import com.github.rafasantos.restapidoc.SpecificationParser;
 import com.github.rafasantos.restdocmaker.RestDocMaker;
 import com.github.rafasantos.restdocmaker.template.Snippet;
 import com.github.rafasantos.restdocmaker.template.SnippetFactory;
 import com.github.rafasantos.restdocmaker.template.TemplateUtil;
 import com.github.rafasantos.restdocmaker.util.JsonUtil;
+import com.matchandtrade.doc.maker.TemplateHelper;
 import com.matchandtrade.doc.util.MatchAndTradeApiFacade;
 import com.matchandtrade.doc.util.MatchAndTradeRestUtil;
 import com.matchandtrade.doc.util.PaginationTemplateUtil;
-import com.matchandtrade.rest.v1.json.TradeJson;
 import com.matchandtrade.rest.v1.json.MembershipJson;
-
-import io.restassured.builder.RequestSpecBuilder;
+import com.matchandtrade.rest.v1.json.TradeJson;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Method;
-import io.restassured.specification.RequestSpecification;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 
 public class MembershipRestDocMaker implements RestDocMaker {
@@ -38,48 +37,99 @@ public class MembershipRestDocMaker implements RestDocMaker {
 	@Override
 	public String content() {
 		String template = TemplateUtil.buildTemplate(contentFilePath());
-		MatchAndTradeApiFacade apiFacade = new MatchAndTradeApiFacade();
-		SnippetFactory snippetFactory = new SnippetFactory(ContentType.JSON, MatchAndTradeRestUtil.nextAuthorizationHeader());
 
 		// MEMBERSHIPS_POST_PLACEHOLDER
-		TradeJson tradeJson = apiFacade.createTrade("Board games in Vancouver - " + new Date().getTime() + hashCode());
-		MembershipJson membership = new MembershipJson();
-		membership.setTradeId(tradeJson.getTradeId());
-		membership.setUserId(MatchAndTradeRestUtil.getLastAuthenticatedUserId());
-		
-		Snippet postSnippet = snippetFactory.makeSnippet(Method.POST, membership, MatchAndTradeRestUtil.membershipsUrl() + "/");
-		postSnippet.getResponse().then().statusCode(201).and().body("membershipId", notNullValue());
-		template = TemplateUtil.replacePlaceholder(template, MEMBERSHIPS_POST_PLACEHOLDER, postSnippet.asHtml());
-		membership = JsonUtil.fromResponse(postSnippet.getResponse(), MembershipJson.class);
+		SpecificationParser postMembershipParser = parserPostMembership();
+		template = TemplateHelper.replacePlaceholder(template, MEMBERSHIPS_POST_PLACEHOLDER, postMembershipParser.asHtmlSnippet());
+		MembershipJson membership = JsonUtil.fromResponse(postMembershipParser.getResponse(), MembershipJson.class);
 
 		// MEMBERSHIPS_GET_PLACEHOLDER
-		Snippet getSnippet = snippetFactory.makeSnippet(MatchAndTradeRestUtil.membershipsUrl(membership.getMembershipId()));
-		getSnippet.getResponse().then().statusCode(200).and().body("membershipId", equalTo(membership.getMembershipId()));
-		template = TemplateUtil.replacePlaceholder(template, MEMBERSHIPS_GET_PLACEHOLDER, getSnippet.asHtml());
+		SpecificationParser getMembershipParser = parseGetMembership(membership);
+		template = TemplateHelper.replacePlaceholder(template, MEMBERSHIPS_GET_PLACEHOLDER, getMembershipParser.asHtmlSnippet());
 
 		// MEMBERSHIPS_GET_ALL_PLACEHOLDER
-		Snippet getAllSnippet = snippetFactory.makeSnippet(MatchAndTradeRestUtil.membershipsUrl());
-		getAllSnippet.getResponse().then().statusCode(200).and().header("X-Pagination-Total-Count", notNullValue());
-		template = TemplateUtil.replacePlaceholder(template, MEMBERSHIPS_GET_ALL_PLACEHOLDER, getAllSnippet.asHtml());
-		
+		SpecificationParser getAllMembershipsParser = parseGetAllMemberships();
+		template = TemplateHelper.replacePlaceholder(template, MEMBERSHIPS_GET_ALL_PLACEHOLDER, getAllMembershipsParser.asHtmlSnippet());
+
 		// MEMBERSHIPS_SEARCH_PLACEHOLDER
-		RequestSpecification searchRequest = new RequestSpecBuilder()
-				.addHeaders(MatchAndTradeRestUtil.getLastAuthorizationHeaderAsMap())
-				.addParam("userId", MatchAndTradeRestUtil.getLastAuthenticatedUserId())
-				.addParam("_pageNumber", "1")
-				.addParam("_pageSize", "3")
-				.build();
-		Snippet searchSnippet = SnippetFactory.makeSnippet(Method.GET, searchRequest, MatchAndTradeRestUtil.membershipsUrl()); 
-		searchSnippet.getResponse().then().statusCode(200).and().headers("X-Pagination-Total-Count", notNullValue());
-		template = TemplateUtil.replacePlaceholder(template, MEMBERSHIPS_SEARCH_PLACEHOLDER, searchSnippet.asHtml());
+		SpecificationParser searchMembershipParser = parseSearchMembership();
+		template = TemplateHelper.replacePlaceholder(template, MEMBERSHIPS_SEARCH_PLACEHOLDER, searchMembershipParser.asHtmlSnippet());
 
 		// MEMBERSHIPS_DELETE_PLACEHOLDER
-		Snippet deleteSnippet = snippetFactory.makeSnippet(Method.DELETE, MatchAndTradeRestUtil.membershipsUrl(membership.getMembershipId()));
-		deleteSnippet.getResponse().then().statusCode(204);
-		template = TemplateUtil.replacePlaceholder(template, MEMBERSHIPS_DELETE_PLACEHOLDER, deleteSnippet.asHtml());
+		SpecificationParser deleteMembershipParser = parseDeleteMembership(membership);
+		template = TemplateHelper.replacePlaceholder(template, MEMBERSHIPS_DELETE_PLACEHOLDER, deleteMembershipParser.asHtmlSnippet());
 
 		template = PaginationTemplateUtil.replacePaginationRows(template);
 		return TemplateUtil.appendHeaderAndFooter(template);
+	}
+
+	private SpecificationParser parseDeleteMembership(MembershipJson membership) {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.delete(MatchAndTradeRestUtil.membershipsUrl(membership.getMembershipId()));
+		parser.getResponse().then().statusCode(204);
+		return parser;
+	}
+
+	private SpecificationParser parseSearchMembership() {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+				.filter(filter)
+				.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+				.queryParam("userId", MatchAndTradeRestUtil.getLastAuthenticatedUserId())
+				.get(MatchAndTradeRestUtil.membershipsUrl());
+		return parser;
+	}
+
+	private SpecificationParser parseGetAllMemberships() {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given().filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.queryParam("_pageNumber", "1")
+			.queryParam("_pageSize", "3")
+			.get(MatchAndTradeRestUtil.membershipsUrl());
+		parser.getResponse().then().statusCode(200);
+		return parser;
+	}
+
+	private SpecificationParser parseGetMembership(MembershipJson membership) {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.get(MatchAndTradeRestUtil.membershipsUrl(membership.getMembershipId()));
+		parser.getResponse().then().statusCode(200).and().body("membershipId", equalTo(membership.getMembershipId()));
+		return parser;
+	}
+
+	private SpecificationParser parserPostMembership() {
+		MembershipJson membership = buildMembership();
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.contentType(ContentType.JSON)
+			.body(membership)
+			.post(MatchAndTradeRestUtil.membershipsUrl() + "/");
+		parser.getResponse().then().statusCode(201).and().body("membershipId", notNullValue());
+		return parser;
+	}
+
+	private MembershipJson buildMembership() {
+		MatchAndTradeApiFacade apiFacade = new MatchAndTradeApiFacade();
+		TradeJson tradeJson = apiFacade.createTrade("Board games in Vancouver - " + System.currentTimeMillis());
+		MatchAndTradeRestUtil.nextAuthorizationHeader();
+		MembershipJson membership = new MembershipJson();
+		membership.setTradeId(tradeJson.getTradeId());
+		membership.setUserId(MatchAndTradeRestUtil.getLastAuthenticatedUserId());
+		return membership;
 	}
 
 }
