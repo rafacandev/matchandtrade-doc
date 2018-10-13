2bea12d274f929f0615e8b9adf5dbb79689e7197
Rafael Santos <rafael.santos.bra@gmail.com>
2018-10-13 16:38:36 -0400

    Refactor UserRestDocMaker to use REST API Doc

--- src/main/java/com/matchandtrade/doc/maker/rest/UserRestDocMaker.java
+++ src/main/java/com/matchandtrade/doc/maker/rest/UserRestDocMaker.java
@@ -4,15 +4,19 @@ package com.matchandtrade.doc.maker.rest;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
 
+import com.github.rafasantos.restapidoc.SpecificationFilter;
+import com.github.rafasantos.restapidoc.SpecificationParser;
 import com.github.rafasantos.restdocmaker.RestDocMaker;
 import com.github.rafasantos.restdocmaker.template.Snippet;
-import com.github.rafasantos.restdocmaker.template.SnippetFactory;
 import com.github.rafasantos.restdocmaker.template.TemplateUtil;
+import com.matchandtrade.doc.maker.TemplateHelper;
 import com.matchandtrade.doc.util.MatchAndTradeRestUtil;
 import com.matchandtrade.rest.v1.json.UserJson;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
+import io.restassured.http.Header;
+import org.hibernate.sql.Template;
 
 
 public class UserRestDocMaker implements RestDocMaker {
@@ -26,23 +30,44 @@ public class UserRestDocMaker implements RestDocMaker {
 
 	@Override
 	public String content() {
-		String template = TemplateUtil.buildTemplate(contentFilePath());
-		SnippetFactory snippetFactory = new SnippetFactory(ContentType.JSON, MatchAndTradeRestUtil.getLastAuthorizationHeader());
-		
+		String template = TemplateHelper.buildTemplate(contentFilePath());
+
 		// USERS_PUT_PLACEHOLDER
+		SpecificationParser putUserParser = parsePutUser();
+		template = TemplateHelper.replacePlaceholder(template, USERS_PUT_PLACEHOLDER, putUserParser.asHtmlSnippet());
+
+		// USERS_GET_PLACEHOLDER
+		SpecificationParser parser = parseGetUser();
+		template = TemplateHelper.replacePlaceholder(template, USERS_GET_PLACEHOLDER, parser.asHtmlSnippet());
+
+		return TemplateUtil.appendHeaderAndFooter(template);
+	}
+
+	private SpecificationParser parseGetUser() {
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.get(MatchAndTradeRestUtil.usersUrl() + "/" + MatchAndTradeRestUtil.getLastAuthenticatedUserId());
+		parser.getResponse().then().statusCode(200);
+		return parser;
+	}
+
+	private SpecificationParser parsePutUser() {
 		UserJson userJson = new UserJson();
 		userJson.setEmail(MatchAndTradeRestUtil.getLastAuthenticatedUser().getEmail());
 		userJson.setName("Scott Summers");
-		Snippet putSnippet = snippetFactory.makeSnippet(Method.PUT, userJson, MatchAndTradeRestUtil.usersUrl() + "/" + MatchAndTradeRestUtil.getLastAuthenticatedUserId());
-		putSnippet.getResponse().then().statusCode(200).and().body("name", equalTo(userJson.getName()));
-		template = TemplateUtil.replacePlaceholder(template, USERS_PUT_PLACEHOLDER, putSnippet.asHtml());
-
-		// USERS_GET_PLACEHOLDER
-		Snippet getSnippett = snippetFactory.makeSnippet(MatchAndTradeRestUtil.usersUrl() + "/" + MatchAndTradeRestUtil.getLastAuthenticatedUserId());
-		getSnippett.getResponse().then().statusCode(200).and().body("", hasKey("userId"));
-		template = TemplateUtil.replacePlaceholder(template, USERS_GET_PLACEHOLDER, getSnippett.asHtml());
-		
-		return TemplateUtil.appendHeaderAndFooter(template);
+		SpecificationFilter filter = new SpecificationFilter();
+		SpecificationParser parser = new SpecificationParser(filter);
+		RestAssured.given()
+			.filter(filter)
+			.header(MatchAndTradeRestUtil.getLastAuthorizationHeader())
+			.contentType(ContentType.JSON)
+			.body(userJson)
+			.put(MatchAndTradeRestUtil.usersUrl() + "/" + MatchAndTradeRestUtil.getLastAuthenticatedUserId());
+		parser.getResponse().then().statusCode(200).and().body("name", equalTo(userJson.getName()));
+		return parser;
 	}
 
 }
